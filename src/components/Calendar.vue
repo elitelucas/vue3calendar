307c9c01fe9630da3be6bf3391d39e70151eea0d
Vladimir <vovochkaperepelkin@yandex.ru>
2020-08-27 23:34:46 +0900

    Add Calendar.vue

---
+++ src/components/Calendar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>My Event</h1>
+    <p>Capacity: {{ capacity }}</p>
+    <button @click="increaseCapacity()">Increase Capacity</button>
+  </div>
+</template>
+
+<script>
+import { ref } from "@vue/composition-api"; // <-- Use this line if you're in a Vue 2 app with the composition API plugin
+import { ref } from "vue"; // <-- Use this line if you're in a Vue 3 app
+
+export default {
+  setup() {
+    const capacity = ref(3);
+
+    function increaseCapacity() { 
+      capacity.value++;
+    }
+    return { capacity, increaseCapacity };
+  }
+};
+</script>
